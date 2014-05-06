e00df5a951c984c9702c0ae1c56645e4ef2ae958
Vasily Chekalkin <bacek@yandex-team.ru>
2014-05-06 16:15:51 +1000

    Force spdy version in framer

--- phantom/io_benchmark/method_stream/spdy/framer/spdy_framer.C
+++ phantom/io_benchmark/method_stream/spdy/framer/spdy_framer.C
@@ -6,17 +6,32 @@
 
 namespace phantom { namespace io_benchmark {
 
+MODULE(io_benchmark_method_stream_spdy_framer);
+
 namespace method_stream {
 config_binding_sname(spdy_framer_t);
+config_binding_value(spdy_framer_t, spdy_version);
 config_binding_ctor(spdy_framer_t, spdy_framer_t);
 }
 
-MODULE(io_benchmark_method_stream_spdy_framer);
+config_enum_internal_sname(spdy_framer_t, spdy_version_t);
+config_enum_internal_value(spdy_framer_t, spdy_version_t, spdy2);
+config_enum_internal_value(spdy_framer_t, spdy_version_t, spdy3);
+config_enum_internal_value(spdy_framer_t, spdy_version_t, spdy3_1);
 
 spdy_framer_t::spdy_framer_t() {}
 
-spdy_framer_t::spdy_framer_t(string_t const &, config_t const &) {}
+spdy_framer_t::spdy_framer_t(string_t const &, config_t const &config)
+    : spdy_version(config.spdy_version) {
+}
 
 spdy_framer_t::~spdy_framer_t() {}
+
+
+
+void spdy_framer_t::config_t::check(in_t::ptr_t const &ptr) const {
+    if (!spdy_version)
+        config::error(ptr, "'spdy_version' is required. Try to put 'spdy3_1'");
+}
 
 }}  // namespace phantom::io_benchmark
