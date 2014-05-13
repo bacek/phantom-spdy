eff2321f905dee2e0bc1c8bee024391518c0fe90
Vasily Chekalkin <bacek@yandex-team.ru>
2014-05-13 11:10:10 +1000

    Cleanup spdy_framer_t a bit.

--- phantom/io_benchmark/method_stream/spdy/spdy_framer.C
+++ phantom/io_benchmark/method_stream/spdy/spdy_framer.C
@@ -11,75 +11,8 @@
 
 namespace phantom { namespace io_benchmark { namespace method_stream {
 
-namespace {
-ssize_t do_send(spdylay_session* UNUSED(session),
-            const uint8_t* data, size_t length, int UNUSED(flags),
-            void *user_data) {
-    log_debug("SPDY: framer sending data %ld", length);
-    spdy_framer_t* self = static_cast<spdy_framer_t*>(user_data);
-
-    string_t::ctor_t buf(length);
-    for (size_t i = 0; i < length; ++i)
-        buf(*data++);
-    string_t s = buf;
-    self->send_buffer.append(s);
-
-    return length;
-}
-
-void on_ctrl_recv_callback(spdylay_session* UNUSED(session),
-                              spdylay_frame_type type,
-                              spdylay_frame* frame,
-                              void* user_data) {
-    spdy_framer_t* self = static_cast<spdy_framer_t*>(user_data);
-
-    log_debug("SPDY: Got CONTROL frame type %d %d %ld",
-        type,
-        frame->ctrl.hd.flags,
-        self->in_flight_requests);
-
-    if (type == SPDYLAY_SYN_REPLY) {
-        if (frame->syn_reply.hd.flags & SPDYLAY_CTRL_FLAG_FIN)
-            self->in_flight_requests--;
-
-        // Search for :status and extract it
-        // Iterate over even headers.
-        for (ssize_t i = 0; frame->syn_reply.nv[i]; i +=2 ) {
-            if (strncmp(frame->syn_reply.nv[i], ":status", 7) == 0) {
-                char* end;
-                self->last_res_code = strtoul(frame->syn_reply.nv[i+1], &end, 10);
-                if (*end != ' ') {
-                    log_debug("Failed to parse status line");
-                    self->last_res_code = 500;
-                }
-                log_debug("SPDY: status '%d'", self->last_res_code);
-                break;
-            }
-        }
-    } else if (type == SPDYLAY_GOAWAY) {
-        // It's not actually true. But it will work for now
-        self->last_res_code = 500;
-    }
-}
-
-void on_data_recv_callback(spdylay_session* UNUSED(session),
-                           uint8_t flags,
-                           int32_t stream_id,
-                           int32_t length,
-                           void* user_data) {
-    spdy_framer_t* self = static_cast<spdy_framer_t*>(user_data);
-    log_debug("SPDY: Got DATA frame for stream %d length %d flags %d (%ld)",
-            stream_id,
-            length,
-            flags,
-            self->in_flight_requests);
-    if (flags & SPDYLAY_DATA_FLAG_FIN)
-        self->in_flight_requests--;
-}
-
-}
-
-spdy_framer_t::spdy_framer_t(const ssl_ctx_t& c) : spdy_version(spdy3_1), ctx(c), session(nullptr), in_flight_requests(0) {}
+spdy_framer_t::spdy_framer_t(const ssl_ctx_t& c)
+    : spdy_version(spdy3_1), ctx(c), session(nullptr), in_flight_requests(0) {}
 
 spdy_framer_t::~spdy_framer_t() {
     log_debug("SPDY: destructing framer %lx", this);
@@ -139,6 +72,71 @@ bool spdy_framer_t::send_data(in_segment_t &out) {
     send_buffer.clear();
     log_debug("SPDY: send_data %lu", out.size());
     return true;
+}
+
+ssize_t spdy_framer_t::do_send(spdylay_session* UNUSED(session),
+            const uint8_t* data, size_t length, int UNUSED(flags),
+            void *user_data) {
+    log_debug("SPDY: framer sending data %ld", length);
+    spdy_framer_t* self = static_cast<spdy_framer_t*>(user_data);
+
+    string_t::ctor_t buf(length);
+    for (size_t i = 0; i < length; ++i)
+        buf(*data++);
+    string_t s = buf;
+    self->send_buffer.append(s);
+
+    return length;
+}
+
+void spdy_framer_t::on_ctrl_recv_callback(spdylay_session* UNUSED(session),
+                              spdylay_frame_type type,
+                              spdylay_frame* frame,
+                              void* user_data) {
+    spdy_framer_t* self = static_cast<spdy_framer_t*>(user_data);
+
+    log_debug("SPDY: Got CONTROL frame type %d %d %ld",
+        type,
+        frame->ctrl.hd.flags,
+        self->in_flight_requests);
+
+    if (type == SPDYLAY_SYN_REPLY) {
+        if (frame->syn_reply.hd.flags & SPDYLAY_CTRL_FLAG_FIN)
+            self->in_flight_requests--;
+
+        // Search for :status and extract it
+        // Iterate over even headers.
+        for (ssize_t i = 0; frame->syn_reply.nv[i]; i +=2 ) {
+            if (strncmp(frame->syn_reply.nv[i], ":status", 7) == 0) {
+                char* end;
+                self->last_res_code = strtoul(frame->syn_reply.nv[i+1], &end, 10);
+                if (*end != ' ') {
+                    log_debug("Failed to parse status line");
+                    self->last_res_code = 500;
+                }
+                log_debug("SPDY: status '%d'", self->last_res_code);
+                break;
+            }
+        }
+    } else if (type == SPDYLAY_GOAWAY) {
+        // It's not actually true. But it will work for now
+        self->last_res_code = 500;
+    }
+}
+
+void spdy_framer_t::on_data_recv_callback(spdylay_session* UNUSED(session),
+                           uint8_t flags,
+                           int32_t stream_id,
+                           int32_t length,
+                           void* user_data) {
+    spdy_framer_t* self = static_cast<spdy_framer_t*>(user_data);
+    log_debug("SPDY: Got DATA frame for stream %d length %d flags %d (%ld)",
+            stream_id,
+            length,
+            flags,
+            self->in_flight_requests);
+    if (flags & SPDYLAY_DATA_FLAG_FIN)
+        self->in_flight_requests--;
 }
 
 }}}  // namespace phantom::io_benchmark::method_stream
