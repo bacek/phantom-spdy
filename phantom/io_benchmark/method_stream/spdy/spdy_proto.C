4b0c5eb168d73d19a5ee80c53cae8464a38ec653
Vasily Chekalkin <bacek@yandex-team.ru>
2014-05-13 12:33:00 +1000

    Merge spdy_proto.H and .C.
    It's implementation details and no one use from outside.

--- phantom/io_benchmark/method_stream/spdy/spdy_proto.C
+++ phantom/io_benchmark/method_stream/spdy/spdy_proto.C
@@ -1,11 +1,39 @@
-#include "spdy_proto.H"
+// Copyright (c) 2014 Yandex LLC. All rights reserved.
+// Author: Vasily Chekalkin <bacek@yandex-team.ru>
 
 #include <pd/base/config.H>
 
+#include <phantom/io_benchmark/method_stream/proto.H>
+#include <phantom/io_benchmark/method_stream/logger.H>
+
+
+#include "spdy_framer.H"
 #include "spdy_transport.H"
 #include "spdy_misc.H"
 
 namespace phantom { namespace io_benchmark { namespace method_stream {
+
+class spdy_proto_t : public proto_t {
+public:
+    // We have to have "config" for loadable module.
+    struct config_t {
+		void check(in_t::ptr_t const &) const {}
+    };
+
+    // proto_t implementation
+	virtual bool reply_parse(
+		in_t::ptr_t &ptr, in_segment_t const &request,
+		unsigned int &res_code, logger_t::level_t &lev
+	) const;
+
+    // shared_t implementation
+	virtual void do_init() {};
+	virtual void do_run() const {};
+	virtual void do_stat_print() const {};
+	virtual void do_fini() {};
+
+    spdy_proto_t(string_t const &sname, config_t const &config);
+};
 
 spdy_proto_t::spdy_proto_t(string_t const &sname, config_t const &)
     : proto_t(sname) {
