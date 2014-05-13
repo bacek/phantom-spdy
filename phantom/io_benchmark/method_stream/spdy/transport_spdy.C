ab5044bd5946b7e8d13eda5ff7c15fe97f4be18d
Vasily Chekalkin <bacek@yandex-team.ru>
2014-05-13 10:08:33 +1000

    Move transport_spdy_t::conn_t into C file to hide it from everyone else.
    It's implementation details. And no one should care about it.

--- phantom/io_benchmark/method_stream/spdy/transport_spdy.C
+++ phantom/io_benchmark/method_stream/spdy/transport_spdy.C
@@ -10,7 +10,52 @@ namespace phantom { namespace io_benchmark { namespace method_stream {
 
 MODULE(io_benchmark_method_stream_spdy);
 
-namespace transport_spdy {
+namespace {
+    // Thread local SPDY framer
+    bq_spec_decl(spdy_framer_t, cur_framer);
+
+    // Connection for SPDY.
+    // It's created thread local by method_stream_t.
+    // So we piggy back on it and create SPDY framer alongside.
+	class conn_ssl_t : public conn_t {
+		bq_conn_ssl_t bq_conn_ssl;
+
+		virtual operator bq_conn_t &() {
+            return bq_conn_ssl;
+        }
+
+	public:
+		inline conn_ssl_t(
+			int fd, ssl_ctx_t const &ctx, interval_t _timeout, fd_ctl_t const *_ctl
+		) : conn_t(fd), bq_conn_ssl(fd, ctx, _timeout, _ctl) {
+            // Reset framer for new con
+            delete cur_framer;
+            cur_framer = new spdy_framer_t(ctx);
+        }
+
+		virtual ~conn_ssl_t() throw() {
+            delete cur_framer;
+        }
+	};
+}
+
+transport_spdy_t::~transport_spdy_t() throw() {}
+
+conn_t *transport_spdy_t::new_connect(int fd, fd_ctl_t const *_ctl) const {
+    return new conn_ssl_t(fd, ctx, timeout, _ctl);
+}
+
+spdy_framer_t* transport_spdy_t::current_framer() {
+    return cur_framer;
+}
+
+}}}  // namespace phantom::io_benchmark::method_stream
+
+namespace pd { namespace config {
+
+using phantom::io_benchmark::method_stream::transport_spdy_t;
+using phantom::io_benchmark::method_stream::transport_t;
+
 config_binding_sname(transport_spdy_t);
 config_binding_type(transport_spdy_t, auth_t);
 config_binding_value(transport_spdy_t, auth);
@@ -19,33 +64,6 @@ config_binding_value(transport_spdy_t, timeout);
 config_binding_value(transport_spdy_t, protos);
 config_binding_cast(transport_spdy_t, transport_t);
 config_binding_ctor(transport_t, transport_spdy_t);
-}
 
-namespace {
-    __thread spdy_framer_t* cur_framer;
-}
+}}  // namespace pd::config
 
-transport_spdy_t::~transport_spdy_t() throw() {
-    delete cur_framer;
-}
-
-conn_t *transport_spdy_t::new_connect(int fd, fd_ctl_t const *_ctl) const {
-    auto* res = new conn_ssl_t(fd, ctx, timeout, _ctl);
-    // Reset framer for new con
-    delete cur_framer;
-    cur_framer = new spdy_framer_t(ctx);
-    log_debug("SPDY: creating framer %lx", cur_framer);
-	return res;
-}
-
-transport_spdy_t::conn_ssl_t::operator bq_conn_t &() {
-	return bq_conn_ssl;
-}
-
-transport_spdy_t::conn_ssl_t::~conn_ssl_t() throw() { }
-
-spdy_framer_t* transport_spdy_t::current_framer() {
-    return cur_framer;
-}
-
-}}}  // namespace yandex
